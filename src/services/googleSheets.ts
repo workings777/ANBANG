3e190666b05c3103d90db00d4559b60f8bbb39bc
workings777 <fursysps@gmail.com>
2026-03-19 16:51:33 +0900

    Update googleSheets.ts

--- src/services/googleSheets.ts
+++ src/services/googleSheets.ts
@@ -4,7 +4,7 @@ import { MonthlyData, ProfitabilityData } from '../data/mockData';
 
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaFKe2m2x6HyEePar5T_yE4xTAzJ5QFs2pveVPM0SJXiKr0QrJoEYiaCAJ4L3-HROBj51_kAwlUXq6/pub?gid=1092502501&single=true&output=csv';
 const PROFITABILITY_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaFKe2m2x6HyEePar5T_yE4xTAzJ5QFs2pveVPM0SJXiKr0QrJoEYiaCAJ4L3-HROBj51_kAwlUXq6/pub?gid=1722593857&single=true&output=csv';
-const ANBANG_ANALYSIS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaFKe2m2x6HyEePar5T_yE4xTAzJ5QFs2pveVPM0SJXiKr0QrJoEYiaCAJ4L3-HROBj51_kAwlUXq6/pub?gid=863618794&single=true&output=csv';
+const ANBANG_ANALYSIS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSWffDXRNPP-82jsG1CrEOqySnz3-Qsoh36n3B_DhPTu7dSVzBziXCdfUC4IAEM06HXq33OYJM6Zabo/pub?gid=0&single=true&output=csv';
 const GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzIZDcdDiPTfuGCRSw5sIp-Dyg6Zz2pF-njf3DUhyes84Ydrki4P6niYNiz6F_q4_wA/exec';
 
 
